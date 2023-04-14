eb240c9e7deb2506fea414ab72f4749872099ca5
leilen <leilen@naver.com>
2023-04-14 15:25:23 +0900

    asdasd

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ async function run() {
   //const octokit = github.getOctokit('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');
   const currentBranch = core.getInput('current_branch');
   core.debug(`testaaaaa ${currentBranch}`);
+  console.log(11111);
   console.log(currentBranch);
 }
 
