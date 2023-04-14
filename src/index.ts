dd2ddf7809d52ceb12f45d92885d989eb49d84c2
leilen <leilen@naver.com>
2023-04-14 14:49:27 +0900

    aaaaaa

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
 import * as github from '@actions/github';
+import * as core from '@actions/core';
 async function run() {
   const octokit = github.getOctokit('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');
-  const packages = await octokit.rest.packages.getPackageForUser({
-    username: 'leilen',
-    package_type: 'npm',
-    package_name: 'action-test',
-  });
-  console.log(packages);
+  core.debug('testaaaaa');
 }
 
 run();
