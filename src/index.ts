b7445a0b02e26eba5f9dd7d41059177659996edd
leilen <leilen@naver.com>
2023-04-14 14:53:51 +0900

    vvdsvd

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as github from '@actions/github';
 import * as core from '@actions/core';
 async function run() {
-  const octokit = github.getOctokit('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');
+  //const octokit = github.getOctokit('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');
   core.debug('testaaaaa');
 }
 
